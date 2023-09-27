b36c6854e0d93651a7885d2f0d47752288a6d800
Alireza Arvandi <arvandi@gmail.com>
2023-09-26 18:22:05 -0600

    curroute added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from './components/Header';
+import { useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const curroute = (useLocation().pathname);
   return (
     <div className="App">
       <Header curroute={curroute} />
