ef9bf7a87997594287b2ddbe3867041285bfa9c9
Alireza Arvandi <arvandi@gmail.com>
2023-09-26 18:10:14 -0600

    css changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <p> salam </p>
+        <p> salam </p>
+        <p> salam </p>
+        <li> salam </li>
+        <li> salam </li>
       </header>
     </div>
   );
