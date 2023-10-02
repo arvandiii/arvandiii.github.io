62928134a02780d51d898fc66a7e9597c588b7cf
Alireza Arvandi <alireza.arv.mail@gmail.com>
2023-10-02 15:42:46 -0600

    github nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GitIcon from '../assets/github-mark.svg'
 
 export default function Header() {
     let head = 'Alireza Arvandi';
     return (
-        <div className="window">
-            <div className="title-bar header">
-                <Link className="title a" to="/">
-                    <h1>{head}</h1>
-                </Link>
+        <div>
+            <div class="navbar">
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <Link class="navbar-item" to="/">
+                            <h1>{head}</h1>
+                        </Link>
+                    </div>
+                    <div class="navbar-end">
+                        <Link class="navbar-item" to="/blog">All Blogs</Link>
+                        <Link class="navbar-item" to="https://github.com/arvandiii">
+                            <img src={GitIcon} alt="Github" />
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
