db9fe929e0cc7ec3c945da77b191ebc46153a06e
Alireza Arvandi <arvandi@gmail.com>
2023-10-02 14:59:03 -0600

    small change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Header />
-      salam
+      <Link className="links display-links" to="/blog">All Blogs</Link>
     </div>
   );
 }
